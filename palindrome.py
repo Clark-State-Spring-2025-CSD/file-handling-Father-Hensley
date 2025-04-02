d11137fc685999e4f523ef8a3dcca0e522f56901
Father-Hensley <jhensley0818@gmail.com>
2025-04-02 13:46:44 -0400

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -3,3 +3,23 @@
 #For example, wow is a paldindrome.
 #A different file wile be used for grading.
 #Correct answer for this file: 
+
+f = open("words.txt", "r") #Imports from text file
+words = [line.strip() for line in f] #Fuction that calls for a list and strips 
+#whitespace for ease of reading
+def palindrome(word: str) -> bool: #Def fuction, checking if word is palindrome
+    #Removes non-alphabetical characters and makes all lowercase
+    cleaned_text = ''.join(char.lower() for char in word if char.isalpha())
+    return cleaned_text == cleaned_text[::-1] #Checks if matching forward and reverse
+    #[::-1] reverses the string
+
+count = 0 #Starting count at zero
+
+for word in words: #Checks the list to count
+    if palindrome(word): #Runs def fuction, checking each word
+        count += 1 #Adds one when found
+
+total = count #Storing the count
+
+print(f"There are {total} palindromes in the list.") #Display result
+f.close() #Closing file after running
