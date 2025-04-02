7c141f3e8ada0c07fb5da246eda6de078d4c3877
Father-Hensley <jhensley0818@gmail.com>
2025-04-02 14:23:55 -0400

    Update fileecho.py

--- fileecho.py
+++ fileecho.py
@@ -1,4 +1,11 @@
 #Read in the echo.txt file and display it in the terminal using print()
 #This is a guided practice. Either follow the video or your instructor in class.
-f = open("echo.txt", "r")
-print(f.read())
+curFile = open("echo.txt") #Imports from text file
+#curLine = curFile.readline() #Reads the first line
+#curLine = curFile.read() #Reads entire file
+for curLine in curFile: #Creates a loop checing each line in the file
+    print(curLine) #Prints echo.txt in the terminal
+
+curFile.close() #Closes text file
+
+#print(curLine) #Prints echo.txt in the terminal
